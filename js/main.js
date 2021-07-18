d54f6daddb61823e049b908ed98f830cbe2325d4
Bratonatz <bratonatz92@googlemail.com>
2021-07-18 14:54:24 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,61 @@
 (function() {
 	let idleMonster;
-	let dungeonTileSet2 = new Image();
-	dungeonTileSet2.src = "assets/tiles/0x72_DungeonTilesetII_v1.3.1/0x72_DungeonTilesetII_v1.3.png";
+	let wizardIdleAnim;
+
+	// Canvas
 	let loaderContext;
 	let loaderCanvas;
 	let loaderBackgroundCanvas;
 	let loaderBackgroundCanvasContext;
+
+	// Animation states
 	let runLoader = true;
-	
+	let wizardIdleAnimRun = true;
+	let wizardAttackAnimRun = false;
+	let attackAnimationRun = false;
+	let spinAttackAnimationRun = false;
+	let walkAnimationRun = false;
+	let tauntAnimationRun = false;
+
+	// Images
+	let dungeonTileSet2 = new Image();
+	dungeonTileSet2.src = "assets/tiles/0x72_DungeonTilesetII_v1.3.1/0x72_DungeonTilesetII_v1.3.png";
+	let wizardIdle = new Image();
+	wizardIdle.src = "assets/images/Wizard Pack/Idle.png";
+	let wizardAttack = new Image();
+	wizardAttack.src = "assets/images/Wizard Pack/Attack1.png";
+	let wizardSpeechBubble = new Image();
+	wizardSpeechBubble.src = "assets/images/Wizard Pack/wizard_speech_bubble.png";
+	let wallMid = new Image();
+	wallMid.src = 'assets/tiles/wall_mid.png';
+	let wallCornerRight = new Image();
+	wallCornerRight.src = 'assets/tiles/wall_corner_right.png';
+	let wallTopMid = new Image();
+	wallTopMid.src = 'assets/tiles/wall_top_mid.png';
+	let mainCharIdle = new Image();
+	mainCharIdle.src = 'assets/images/mainChar/spr_Idle_strip.png';
+	let mainCharWalking = new Image();
+	mainCharWalking.src = 'assets/images/mainChar/spr_Walk_strip.png';
+	let mainCharAttacking = new Image();
+	mainCharAttacking.src = 'assets/images/mainChar/spr_Attack_strip.png';
+	let mainCharTaunting = new Image();
+	mainCharTaunting.src = 'assets/images/mainChar/spr_Taunt_strip.png';
+	let mainCharSpinAttacking = new Image();
+	mainCharSpinAttacking.src = 'assets/images/mainChar/spr_SpinAttack_strip.png';
+
+	// room minimal sizes - max sizes are define on load and on resize
+	// [width, height, leftTopPointX, leftTopPointY]
+	let middleRoomRectMinSize = [18, 9];
+	let middleRoomRectMaxSize = [];
+	let topRoomRectMinSize = [18, 9];
+	let topRoomRectMaxSize = [];
+	let bottomRoomRectMinSize = [18, 9];
+	let bottomRoomRectMaxSize = [];
+	let leftRoomRectMinSize = [9, 18];
+	let leftRoomRectMaxSize = [];
+	let rightRoomRectMinSize = [9, 18];
+	let rightRoomRectMaxSize = [];
+
     window.onload = function()
 	{
 		let floorImages = [];
@@ -36,12 +84,25 @@
 		floor_8_img.src = 'assets/tiles/floor_8.png';
 		floorImages.push(floor_8_img);
 
-		//loader background canvas
+		// define max room sizes
+		middleRoomRectMaxSize[0] = Math.ceil(window.innerWidth / 5 / 16);
+		middleRoomRectMaxSize[1] = Math.ceil(middleRoomRectMaxSize[0] / 2 / 16);
+		topRoomRectMaxSize[0] = Math.ceil(window.innerWidth / 5 / 16);
+		topRoomRectMaxSize[1] = Math.ceil(topRoomRectMaxSize[0] / 2 / 16);
+		bottomRoomRectMaxSize[0] = Math.ceil(window.innerWidth / 5 / 16);
+		bottomRoomRectMaxSize[1] = Math.ceil(bottomRoomRectMaxSize[0] / 2 / 16);
+		leftRoomRectMaxSize[0] = Math.ceil(window.innerWidth / 5 / 16);
+		leftRoomRectMaxSize[1] = Math.ceil(leftRoomRectMaxSize[0] / 2 / 16);
+		rightRoomRectMaxSize[0] = Math.ceil(window.innerWidth / 5 / 16);
+		rightRoomRectMaxSize[1] = Math.ceil(rightRoomRectMaxSize[0] / 2 / 16);
+
+		// loader background canvas
 		loaderBackgroundCanvas = document.querySelector(".loaderBackgroundCanvas");
 		loaderBackgroundCanvasContext = loaderBackgroundCanvas.getContext('2d');
 		loaderBackgroundCanvas.width = window.innerWidth;
 		loaderBackgroundCanvas.height = window.innerHeight;
 
+		// loading overlay tiles
 		floor_1_img.onload = function() {
 			let pattern = loaderBackgroundCanvasContext.createPattern(floor_1_img, 'repeat');
 			loaderBackgroundCanvasContext.fillStyle = pattern;
@@ -70,19 +131,41 @@
 		let columns = Math.ceil(window.innerWidth/16);
 		let rows = Math.ceil(window.innerHeight/16);
 		let tilemapMatrix = [];
+		middleRoomRectMinSize[2] = Math.ceil(columns / 2 - middleRoomRectMinSize[0] / 2);
+		middleRoomRectMinSize[3] = Math.ceil(rows / 2 - middleRoomRectMinSize[1] / 2);
 
 		floorImages[floorImages.length-1].onload = function() {
 			for(i=0; i < rows; i++) {
 				tilemapMatrix[i] = [];
 				for(j=0; j < columns; j++) {
 					let randomNum = Math.random();
-					if(randomNum <= 0.9 && randomNum >= 0) {
-						tilemapMatrix[i].push(floorImages[0]);
-						drawBackgroundOnImagesLoad(backgroundCanvasContext, floorImages[0], i, j);
+					// wall from left to right
+					if (i == 7 && (j == 0 || j == 1 || j == 2 || j == 3 || j == 4)) {
+						tilemapMatrix[i].push(wallMid);
+						drawBackgroundOnImagesLoad(backgroundCanvasContext, wallMid, i, j);
+						drawBackgroundOnImagesLoad(backgroundCanvasContext, wallTopMid, i-1, j);
+					} // wall from top to bottom
+					else if (j == 4 && (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7)) {
+						tilemapMatrix[i].push(wallCornerRight);
+						drawBackgroundOnImagesLoad(backgroundCanvasContext, wallCornerRight, i, j);
+					} // draw the rooms
+					else if ((j >= middleRoomRectMinSize[2] && j <= middleRoomRectMinSize[2] + middleRoomRectMinSize[0] && i == middleRoomRectMinSize[3])
+						|| (i >= middleRoomRectMinSize[3] && i <= middleRoomRectMinSize[3] + middleRoomRectMinSize[1] && j == middleRoomRectMinSize[2])
+						|| (j >= middleRoomRectMinSize[2] && j <= middleRoomRectMinSize[2] + middleRoomRectMinSize[0] && i == middleRoomRectMinSize[3] + middleRoomRectMinSize[1])
+						|| (i >= middleRoomRectMinSize[3] && i <= middleRoomRectMinSize[3] + middleRoomRectMinSize[1] && j == middleRoomRectMinSize[2] + middleRoomRectMinSize[0])) {
+						// middle/start room
+						tilemapMatrix[i].push(wallMid);
+						drawBackgroundOnImagesLoad(backgroundCanvasContext, wallMid, i, j);
+						drawBackgroundOnImagesLoad(backgroundCanvasContext, wallTopMid, i - 1, j);
 					} else {
-						let randomTile = Math.floor(Math.random() * Math.floor(6) + 1);
-						tilemapMatrix[i].push(floorImages[randomTile]);
-						drawBackgroundOnImagesLoad(backgroundCanvasContext, floorImages[randomTile], i, j);
+						if (randomNum <= 0.9 && randomNum >= 0) {
+							tilemapMatrix[i].push(floorImages[0]);
+							drawBackgroundOnImagesLoad(backgroundCanvasContext, floorImages[0], i, j);
+						} else {
+							let randomTile = Math.floor(Math.random() * Math.floor(6) + 1);
+							tilemapMatrix[i].push(floorImages[randomTile]);
+							drawBackgroundOnImagesLoad(backgroundCanvasContext, floorImages[randomTile], i, j);
+						}
 					}
 				}
 			}
@@ -90,44 +173,174 @@
 
 		idleMonster = new GameObject(dungeonTileSet2, 16, 364, 4*32, 36, 90, 4);
 
-		mainCharacter = new GameObject(dungeonTileSet2, 128, 100, 4 * 16, 28, 90, 4, Math.ceil(columns/2*16), Math.ceil(rows/16*16));
+		mainCharacter = new GameObject(mainCharIdle, 0, 0, 2720, 96, 50, 16);
+
+		wizardIdleAnim = new GameObject(wizardIdle, 0, 0, 1386, 190, 90, 6, 1, mainCanvas.height-190);
+
+		wizardAttackAnim = new GameObject(wizardAttack, 0, 0, 1848, 190, 90, 8, 1, mainCanvas.height - 190);
 
 		animateLoadWriting();
 		loaderLoop();
 
 		// EVENTS
+		//Menu click
 		document.querySelector(".swordBurgerMenu").addEventListener("click", function () {
 			this.classList.toggle('open');
 			document.querySelector(".navigation").classList.toggle('open');
 			document.querySelector(".settings").classList.remove('open');
 			document.querySelector(".gearSettings").classList.remove('open');
-		}); 
+		});
+		//Settings click
 		document.querySelector(".gearSettings").addEventListener("click", function () {
 			this.classList.toggle('open');
 			document.querySelector(".settings").classList.toggle('open');
 			document.querySelector(".navigation").classList.remove('open');
 			document.querySelector(".swordBurgerMenu").classList.remove('open');
-		}); 
+		});
+		document.querySelector("#tilemapMainGame").addEventListener("click", function (event) {
+			if(pointIsInClickableRectangle(wizardIdleAnim, event)) {
+				wizardIdleAnimRun = false;
+				wizardAttackAnimRun = true;
+			}
+		});
+		// keybinding
+		document.onkeydown = function (e) {
+			switch (e.which) {
+				case 37: // left
+					walkAnimationRun = true;
+					mainCharacter = new GameObject(mainCharWalking, 0, 0, 1360, 96, 50, 8, mainCharacter.canvasCoordinateX - 16, mainCharacter.canvasCoordinateY);
+					break;
+
+				case 38: // up
+					walkAnimationRun = true;
+					mainCharacter = new GameObject(mainCharWalking, 0, 0, 1360, 96, 50, 8, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY - 16);
+					break;
+
+				case 39: // right
+					walkAnimationRun = true;
+					mainCharacter = new GameObject(mainCharWalking, 0, 0, 1360, 96, 50, 8, mainCharacter.canvasCoordinateX + 16, mainCharacter.canvasCoordinateY);
+					break;
+
+				case 40: // down
+					walkAnimationRun = true;
+					mainCharacter = new GameObject(mainCharWalking, 0, 0, 1360, 96, 50, 8, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY + 16);
+					break;
+
+				case 65: // a - attack
+					attackAnimationRun = true;
+					mainCharacter = new GameObject(mainCharAttacking, 0, 0, 5100, 96, 50, 30, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY);
+					break;
+
+				case 84: // t - taunt
+					tauntAnimationRun = true;
+					mainCharacter = new GameObject(mainCharTaunting, 0, 0, 3060, 96, 50, 18, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY);
+					break;
+
+				case 83: // t - taunt
+					spinAttackAnimationRun = true;
+					mainCharacter = new GameObject(mainCharSpinAttacking, 0, 0, 5100, 96, 50, 30, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY);
+					break;
+
+				default: return; // exit this handler for other keys
+			}
+			e.preventDefault();
+		};
 	};
 
-	function drawBackgroundOnImagesLoad(backgroundCanvasContext, floorImage, i, j) {
-		backgroundCanvasContext.drawImage(floorImage,0,0,16,16,16*j,16*i,16,16);
-	}
-
 	// Main Game Loop
-	function mainCharIdleLoop() {
-		mainCharIdleUpdate();
-		mainCharIdleDraw();
-		requestAnimationFrame(mainCharIdleLoop);
+	function mainLoop() {
+		mainUpdate();
+		mainDraw();
+
+		if (wizardIdleAnimRun) {
+			mainCanvasContext.drawImage(wizardSpeechBubble, 0, 0, 1071, 234, 20, mainCanvas.height - 190, 300, 50);
+		}
+
+		requestAnimationFrame(mainLoop);
 	}
-	function mainCharIdleUpdate(){
+	function mainUpdate(){
+		//main char
 		mainCharacter.update();
+
+		//wizard
+		if (wizardIdleAnimRun){
+			wizardIdleAnim.update();
+		}
+		if (wizardAttackAnimRun) {
+			wizardAttackAnim.update();
+		}
+		
 	}
-	function mainCharIdleDraw() {
+	function mainDraw() {
+		//main char
 		mainCanvasContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
 		mainCharacter.draw(mainCanvasContext);
+		//attack animation only once
+		if (attackAnimationRun) {
+			if (mainCharacter.frameIndex === 29) {
+				attackAnimationRun = false;
+				mainCharacter = new GameObject(mainCharIdle, 0, 0, 2720, 96, 50, 16, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY);
+			}
+		}
+
+		//walk animation only once
+		if (walkAnimationRun) {
+			if (mainCharacter.frameIndex === 7) {
+				walkAnimationRun = false;
+				mainCharacter = new GameObject(mainCharIdle, 0, 0, 2720, 96, 50, 16, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY);
+			}
+		}
+
+		//taunt animation only once
+		if (tauntAnimationRun) {
+			if (mainCharacter.frameIndex === 17) {
+				tauntAnimationRun = false;
+				mainCharacter = new GameObject(mainCharIdle, 0, 0, 2720, 96, 50, 16, mainCharacter.canvasCoordinateX, mainCharacter.canvasCoordinateY);
+			}
+		}
+
+		//spin attack animation only once
+		if (spinAttackAnimationRun) {
+			if (mainCharacter.frameIndex === 29) {
+				spinAttackAnimationRun = false;
+				mainCharacter = new GameObject(mainCharIdle, 0, 0, 2720, 96, 50, 16, mainCharacter.canvasCoordinateX+32, mainCharacter.canvasCoordinateY);
+			}
+		}
+
+		//wizard
+		if (wizardIdleAnimRun) {
+			wizardIdleAnim.draw(mainCanvasContext);
+		}
+		
+		if (wizardAttackAnimRun) {
+			wizardAttackAnim.draw(mainCanvasContext);
+			if (wizardAttackAnim.frameIndex === 7) {
+				wizardAttackAnimRun = false;
+				window.location.href = '/index_classic.html';
+			}
+		}
 	}
 
+	//The Loader Loop
+	function loaderLoop() {
+		updateLoader();
+		drawLoader();
+		setTimeout(function () {
+			runLoader = false;
+		}, 2500);
+
+		if (runLoader === true) {
+			requestAnimationFrame(loaderLoop);
+		} else {
+			loaderCanvas.classList.add("fadeOut");
+			loaderBackgroundCanvas.classList.add("fadeOut");
+			
+			mainLoop();
+		}
+	}
+	function drawBackgroundOnImagesLoad(backgroundCanvasContext, floorImage, i, j) {
+		backgroundCanvasContext.drawImage(floorImage, 0, 0, 16, 16, 16 * j, 16 * i, 16, 16);
+	}
 	let counter = 0;
 	function animateLoadWriting() {
 		let loadingWriting = document.querySelector(".loadWriting");
@@ -150,23 +363,6 @@
 
 	}
 
-	//The Loader Loop
-	function loaderLoop() {
-		updateLoader();
-		drawLoader();
-		setTimeout(function () {
-			runLoader = false;
-		}, 2500);
-
-		if (runLoader === true) {
-			requestAnimationFrame(loaderLoop);
-		} else {
-			loaderCanvas.classList.add("fadeOut");
-			loaderBackgroundCanvas.classList.add("fadeOut");
-			mainCharIdleLoop();
-		}
-	}
-
 	//update function to update all the GameObjects
 	function updateLoader() {
 		idleMonster.update();
@@ -187,13 +383,16 @@
 		this.height = height;                       //height of spritesheet
 		this.timePerFrame = timePerFrame;           //time in(ms) given to each frame
 		this.numberOfFrames = numberOfFrames || 1;  //number of frames(sprites) in the spritesheet, default 1
+		this.canvasCoordinateX = canvasCoordinateX;
+		this.canvasCoordinateY = canvasCoordinateY;
 
-		if(canvasCoordinateX === 0) {
-			canvasCoordinateX = Math.round(loaderCanvas.width / 2 - this.width / this.numberOfFrames);
+
+		if(this.canvasCoordinateX === 0) {
+			this.canvasCoordinateX = Math.round(loaderCanvas.width / 2 - this.width / this.numberOfFrames / 2);
 		}
 
 		if (canvasCoordinateY === 0) {
-			canvasCoordinateY = Math.round(loaderCanvas.height / 2 - this.height);
+			this.canvasCoordinateY = Math.round(loaderCanvas.height / 2 - this.height / 2);
 		}
 
 		//current frame index pointer
@@ -220,10 +419,29 @@
 							y,
 							this.width/this.numberOfFrames,
 							this.height,
-							canvasCoordinateX,
-							canvasCoordinateY,
+							this.canvasCoordinateX,
+							this.canvasCoordinateY,
 							this.width/this.numberOfFrames,
 							this.height);
+		}
+	}
+
+	//check if point is in rectangle for click event
+	function pointIsInClickableRectangle(clickableFigure, eventObject) {
+		let width = clickableFigure.width / clickableFigure.numberOfFrames;
+		let height = clickableFigure.height;
+		let centerPoint = [];
+		centerPoint["x"] = clickableFigure.canvasCoordinateX + width / 2;
+		centerPoint["y"] = clickableFigure.canvasCoordinateY + height / 2;
+
+		if (eventObject.clientX > centerPoint["x"] - width &&
+			eventObject.clientX < centerPoint["x"] + width &&
+			eventObject.clientY > centerPoint["y"] - height &&
+			eventObject.clientY < centerPoint["y"] + height) {
+				//console.log('click in object');
+				return true;
+		} else {
+			return false;
 		}
 	}
 })();
